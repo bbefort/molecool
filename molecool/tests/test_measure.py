bd2a88ab3747f1f0e885d1bc14a1d5892812c599
Bridgette Befort <bbefort@nd.edu>
2020-12-17 10:27:49 -0600

    Added a test which checks multiple points

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -3,6 +3,7 @@ Tests for the measure module
 '''
 
 #imports
+import pytest
 import molecool
 import numpy as np
 
@@ -27,4 +28,20 @@ def test_calculate_angle():
     
     calculated_angle = molecool.calculate_angle(rA, rB, rC,degrees=True)
     
-    assert calculated_angle == expected_angle
+    assert pytest.approx(calculated_angle) == expected_angle
+
+@pytest.mark.parametrize("pA,pB,pC,expected_angle",
+                         [(np.array([np.sqrt(2)/2,np.sqrt(2)/2,0]),
+                         np.array([0,0,0]),
+                         np.array([1,0,0]),
+                          45),
+                         (np.array([0,0,-1]),
+                         np.array([0,1,0]),
+                         np.array([1,0,0]),
+                         60)] 
+                         )
+def test_calculate_angle_many(pA,pB,pC,expected_angle):
+    calculated_angle = molecool.calculate_angle(pA,pB,pC,degrees=True)
+    
+    assert pytest.approx(calculated_angle) == expected_angle
+    
